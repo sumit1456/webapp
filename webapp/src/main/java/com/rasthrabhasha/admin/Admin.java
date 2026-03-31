f777c5ec98161d6b43de2be6bee33337189c67e9
sumit1456 <sumithatekar067@gmail.com>
2026-03-31 23:49:49 +0530

    d

--- webapp/src/main/java/com/rasthrabhasha/admin/Admin.java
+++ webapp/src/main/java/com/rasthrabhasha/admin/Admin.java
@@ -6,57 +6,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//comment
+
+//comments
 @Entity
 public class Admin {
 
-	
-	
 	public long getAdmin_id() {
 		return admin_id;
 	}
-
 
 	public void setAdmin_id(long admin_id) {
 		this.admin_id = admin_id;
 	}
 
-
 	public String getUsername() {
 		return username;
 	}
-
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	@Id
-	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long admin_id;
-	
-	
+
 	@Column
 	private String username;
-	
-	
+
 	@Column
 	private String password;
-
-
-
-	
-
 
 }
