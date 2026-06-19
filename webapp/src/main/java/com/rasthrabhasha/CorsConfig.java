eda478f819124cfe08ebd027a6730d2ab0a2711b
sumit1456 <sumithatekar067@gmail.com>
2026-06-19 22:28:16 +0530

    cors update

--- webapp/src/main/java/com/rasthrabhasha/CorsConfig.java
+++ webapp/src/main/java/com/rasthrabhasha/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://100.53.20.30:8080", "https://exam-management-sys.netlify.app")
+						.allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://100.53.20.30:8080", "https://exam-management-sys.netlify.app", "https://exam-management-webapp.onrender.com")
 						.allowedMethods("*")
 						.allowedHeaders("*");
 			}
