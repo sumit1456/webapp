f00b275a050dadca67cdd7bafba447497a2c46d9
sumit1456 <sumithatekar067@gmail.com>
2026-07-04 19:30:18 +0530

    Configure CORS allowed origins via env

--- webapp/src/main/java/com/rasthrabhasha/CorsConfig.java
+++ webapp/src/main/java/com/rasthrabhasha/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.rasthrabhasha;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,16 +8,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+	@Value("${app.cors.allowed-origins}")
+	private String[] allowedOrigins;
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://100.53.20.30:8080", "https://exam-management-sys.netlify.app", "https://exam-management-webapp.onrender.com")
+						.allowedOrigins(allowedOrigins)
 						.allowedMethods("*")
 						.allowedHeaders("*");
 			}
 		};
 	}
 }
+
