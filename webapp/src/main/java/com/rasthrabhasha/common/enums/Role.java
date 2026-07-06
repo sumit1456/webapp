f9a39b4a562ed9a4775924a9ea4aac3b8247b06a
sumit1456 <sumithatekar067@gmail.com>
2026-07-06 18:11:19 +0530

    permisson fix

--- webapp/src/main/java/com/rasthrabhasha/common/enums/Role.java
+++ webapp/src/main/java/com/rasthrabhasha/common/enums/Role.java
@@ -8,22 +8,20 @@ public enum Role {
     ADMIN(EnumSet.allOf(Permission.class)),
 
     STUDENT(EnumSet.of(
-        Permission.VIEW_EXAMS,
-        Permission.SUBMIT_APPLICATION,
-        Permission.VIEW_OWN_PROFILE,
-        Permission.VIEW_RESULTS,
-        Permission.UPLOAD_FILES,
-        Permission.EDIT_OWN_PROFILE
-    )),
+            Permission.VIEW_EXAMS,
+            Permission.SUBMIT_APPLICATION,
+            Permission.VIEW_OWN_PROFILE,
+            Permission.VIEW_RESULTS,
+            Permission.UPLOAD_FILES,
+            Permission.EDIT_OWN_PROFILE)),
 
     EXAM_OFFICER(EnumSet.of(
-        Permission.VIEW_STUDENTS,
-        Permission.VIEW_EXAMS,
-        Permission.VIEW_APPLICATIONS,
-        Permission.MANAGE_APPLICATIONS,
-        Permission.PUBLISH_RESULTS,
-        Permission.VIEW_RESULTS
-    ));
+            Permission.VIEW_STUDENTS,
+            Permission.VIEW_EXAMS,
+            Permission.VIEW_APPLICATIONS,
+            Permission.MANAGE_APPLICATIONS,
+            Permission.PUBLISH_RESULTS,
+            Permission.VIEW_RESULTS));
 
     private final Set<Permission> permissions;
 
